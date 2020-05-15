0c328e1638b15189a1175947f1779973042285c8
tpdzawara <t.pdzawara@gmail.com>
2020-05-15 11:53:57 +0200

    pushes

--- api/utils/geocoder.js
+++ api/utils/geocoder.js
@@ -1,11 +1,11 @@
 const NodeGeocoder = require('node-geocoder');
-const dontenv = require('dotenv');
+const dotenv = require('dotenv');
 dotenv.config({path: require('../config/keys.env')});
 
 const options = {
     provider: process.env.GEOCODER_PROVIDER,
     httpAdapter: 'https',
-    apiKey: process.env.GEOCODER_API_KEY,
+    apiKey: process.env.GEOCODER_API_KEY;
     fomatter: null
 }
 
